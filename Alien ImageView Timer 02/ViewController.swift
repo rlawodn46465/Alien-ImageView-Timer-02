16a99922a1b8d619aa617aec003271ba550b3671
dit08 <dit@imac-3.local>
2019-03-28 16:48:51 +0900

    오토 타이머

--- Alien ImageView Timer 02/ViewController.swift
+++ Alien ImageView Timer 02/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var countLabel: UILabel!
+    var count = 1
+    //Timer 객체 선언
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myImageView.image = UIImage(named: "frame\(count).png")
+        countLabel.text = String(count)
     }
-
+    @IBAction func playBtnPressed(_ sender: Any) {
+        if (!myTimer.isValid){
+        myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+        }
+    }
+    @IBAction func pauseBtnPressed(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    @IBAction func stopBtnPressed(_ sender: Any) {
+        myTimer.invalidate()
+        count = 0
+    }
+    
+    //Timer에서 호출되는 함수 정의
+    @objc func doAnimation() {
+        count+=1
+         if count == 6 {
+            count = 1
+        }
+        myImageView.image = UIImage(named: "frame\(count).png")
+        countLabel.text = String(count)
+        
+    }
 
 }
 
