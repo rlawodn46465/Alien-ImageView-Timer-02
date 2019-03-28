ed63eb7c504d3bef75386518a965307d43a1abfd
dit08 <dit@imac-3.local>
2019-03-28 16:12:01 +0900

    Initial Commit

---
+++ Alien ImageView Timer 02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alien ImageView Timer 02
+//
+//  Created by dit08 on 2019. 3. 28..
+//  Copyright © 2019년 dit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
